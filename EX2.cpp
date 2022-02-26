616fed7dd4113ee0fe263ad380f3400113abcaf8
Andrey <89473462+Arty525@users.noreply.github.com>
2022-02-26 15:34:30 +0300

    Add files via upload

---
+++ EX2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int jump(int* n, int stair,int* vars) {
+	if (stair % *n > 0) {
+		cout << "(" << stair / *n << "," << stair % *n << ")";
+	}
+}
+
+int main(){
+	int stair;
+	int n = 3;
+	int vars = 0;
+
+	cout << "Enter stair's number: ";
+	cin >> stair;
+
+	jump(&n, stair, &vars);
+
+	cout << vars;
+
+	return 0;
+}
