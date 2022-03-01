6f4026a6ab336fb47b0c51d1bc8840f0a683e249
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-01 16:43:34 +0300

    Add files via upload

---
+++ EX4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//void even(long& n, int ans) {
+//	
+//	return even(n, ans);
+//}
+
+
+int main() {
+	long number = 9223372036854775806;
+	long* pnumber = &number;
+	int ans = 0;
+	//even(number, ans);
+	cout << &pnumber[0] << " " << &pnumber[1];
+	return 0;
+}
