50e6df97ef6c2a25dc396a5d9f01a0844f6e35f0
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-01 16:58:11 +0300

    Add files via upload

--- EX4.cpp
+++ EX4.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-//void even(long& n, int ans) {
-//	
-//	return even(n, ans);
-//}
+void even(long& n, int ans) {
+	if (n % 10 % 2 == 0) ++ans;
+	n -= n % 10;
+	cout << n << endl;
+	if ((n - (n % 10)) > 0) return even(n, ans);
+	else cout << ans;
+}
 
 
 int main() {
 	long number = 9223372036854775806;
-	long* pnumber = &number;
 	int ans = 0;
-	//even(number, ans);
-	cout << &pnumber[0] << " " << &pnumber[1];
+	even(number, ans);
 	return 0;
 }
