b27b97d39874e73432f385ba65220e7bc53eb8ad
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-01 15:52:34 +0300

    Add files via upload

---
+++ EX3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int jump(int j, int n, int& v, int& c) {
+	if (n == 1) return 1;
+	++c;
+	c < j ? v *= 2 : v = v * 2 - 1;
+	if (c < n - 1) return jump(j, n, v, c);
+	else return v;
+}
+
+int main() {
+	int n;
+	int j;
+	int counter = 0;
+	int vars = 1;
+
+	cout << "Enter number of stair: ";
+	cin >> n;
+	cout << "Enter jump leight: ";
+	cin >> j;
+
+	cout << "Variants: " << jump(j, n, vars, counter);
+
+	return 0;
+}
