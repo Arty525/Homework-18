487a17b0c38eda8ffb032199c7e7671cf96678e6
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-03 15:15:34 +0300

    Add files via upload

--- EX4.cpp
+++ EX4.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-void even(long& n, int ans) {
-	if (n % 10 % 2 == 0) ++ans;
-	n -= n % 10;
+void even(long& n, int& a) {
+	if ((n % 10) % 2 == 0) ++a;
+	n = (n - (n % 10)) / 10;
 	cout << n << endl;
-	if ((n - (n % 10)) > 0) return even(n, ans);
-	else cout << ans;
+	if (n > 0) return even(n, a);
+	else cout << "Number of even digits:" << a;
 }
 
 
