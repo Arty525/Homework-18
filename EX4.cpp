1d3d714c2ce11cb54f66d37774709b028db12550
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-03 15:25:17 +0300

    Update EX4.cpp

--- EX4.cpp
+++ EX4.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void even(long& n, int& a) {
+void even(long long& n, int& a) {
 	if ((n % 10) % 2 == 0) ++a;
 	n = (n - (n % 10)) / 10;
 	cout << n << endl;
@@ -12,7 +12,7 @@ void even(long& n, int& a) {
 
 
 int main() {
-	long number = 9223372036854775806;
+	long long number = 9223372036854775806;
 	int ans = 0;
 	even(number, ans);
 	return 0;
